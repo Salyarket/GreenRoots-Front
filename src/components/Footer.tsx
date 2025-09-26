365af1d1844d8a8a11c9fa882f7a1e30e691a12a
Tarig Mekhazni <tmk-dev@hotmail.com>
2025-09-26 14:57:57 +0200

    adjusting on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,32 +7,23 @@ const Footer = () => {
   return (
     <footer className="bg-brand-darkgreen text-white py-12 px-16">
       {/* Haut du footer en 3 colonnes */}
-      <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-8">
+      <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-8  ">
         {/* Colonne gauche : menu */}
         <div className="flex flex-col gap-2 text-center md:text-left">
-          <Link href="/" className="custom-btn-hover">Accueil</Link>
-          <Link href="/catalogue" className="custom-btn-hover">Catalogue</Link>
-          <Link href="/a-propos" className="custom-btn-hover">À propos</Link>
-          <Link href="/contact" className="custom-btn-hover">Contact</Link>
-          <Link href="/connexion" className="custom-btn-hover">Se connecter</Link>
-          <Link href="/inscription" className="custom-btn-hover">S’inscrire</Link>
-</div>
-
-        {/* Colonne centre : logo + slogan */}
-        <div className="flex flex-col items-center">
-          <Image
-            src="/logo_white.svg"
-            alt="GreenRoots"
-            width={100}
-            height={100}
-            priority
-          />
-          <span className="mt-2">{"Vert l'avenir"}</span>
+          <Link href="/catalogue" className="custom-btn-hover">
+            Catalogue
+          </Link>
+          <Link href="/a-propos" className="custom-btn-hover">
+            À propos
+          </Link>
+          <Link href="/contact" className="custom-btn-hover">
+            Contact
+          </Link>
         </div>
 
         {/* Colonne droite : les réseaux */}
-        <div className="flex flex-col items-center md:items-end gap-2">
-          <p className="font-semibold">NOUS SUIVRE</p>
+        <div className="flex flex-col  items-center gap-2">
+          <p className="font-semibold ">NOUS SUIVRE</p>
           <div className="flex gap-4 text-2xl">
             {/* <Link> sert avant tout au routing interne <a> est préféré si externe car pas de surcouche donc plus léger*/}
             <a
@@ -59,34 +50,28 @@ const Footer = () => {
             >
               <FaLinkedin />
             </a>
-            <a
-              href="https://fr-fr.facebook.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="custom-btn-hover"
-            >
-              <FaFacebook />
-            </a>
           </div>
         </div>
       </div>
 
+      {/* Colonne centre : logo + slogan */}
+      <div className=" flex flex-col items-center my-4">
+        <Image
+          src="/logo_white.svg"
+          alt="GreenRoots"
+          width={100}
+          height={100}
+          priority
+        />
+        <span className="mt-2">{"Vert l'avenir"}</span>
+      </div>
+
       {/* Pages légales en bas */}
-      <div className="mt-8 text-center text-sm space-y-1">
-        <Link href={"/mentions-legales"} 
-        className="custom-btn-hover">Mentions légales -
+      <div className="mt-4 text-center text-sm ">
+        <Link href={"/mentions-legales"}>
+          Mentions légales - Termes et conditions
         </Link>
-        <Link href={"/termes-et-conditions"} 
-        className="custom-btn-hover"> Termes et conditions
-        </Link>
-        <br />
-        <Link href={"/utilisation-des-cookies"} 
-        className="custom-btn-hover">Utilisation des cookies -
-        </Link>
-        <Link href={"/politique-de-confidentialite"} 
-        className="custom-btn-hover"> Politique de confidentialité
-        </Link>
-        <p>Copyright © 2025 GreenRoots</p>
+        <p className="py-1.5">Copyright © 2025 GreenRoots</p>
       </div>
     </footer>
   );
