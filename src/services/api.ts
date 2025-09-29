90299c360535263e8ec2e422296a6c16c7ba2049
adrien.paris123 <adrien.paris123@gmail.com>
2025-09-29 15:56:18 +0200

    product page by id with gallery of pictures

--- src/services/api.ts
+++ src/services/api.ts
@@ -14,3 +14,16 @@ export async function getProducts() {
     throw error; // avec le throw error, next va envoyer automatiquement la page error.tsx
   }
 }
+
+export async function getOneProduct(id: string) {
+  try {
+    const res = await fetch(`${API_URL}/products/with_location/${id}`, { cache: "no-store" });
+    if (!res.ok) {
+      throw new Error(`Erreur API: ${res.status} ${res.statusText}`);
+    }
+    return res.json();
+  } catch (error) {
+    console.error("Erreur API:", error);
+    throw error; // avec le throw error, next va envoyer automatiquement la page error.tsx
+  }
+}
