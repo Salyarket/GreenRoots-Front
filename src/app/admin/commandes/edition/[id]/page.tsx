4a449db01d25b19270d25e934a5002a4cc92a53c
Saliha <sharkat@duck.com>
2025-10-01 15:37:20 +0200

    Saliha: WIP: modification on package & package.lock

--- src/app/admin/commandes/edition/[id]/page.tsx
+++ src/app/admin/commandes/edition/[id]/page.tsx
@@ -2,7 +2,7 @@ const Page = async () => {
 
     return (
         <main className="min-h-screen mt-16 px-4 custom-size-minmax">
-            <div>Formulaire de modification d'une comande</div>
+            <div>Formulaire de modification d'une commande</div>
         </main>
     );
 };
--- src/app/admin/utilisateurs/profil/page.tsx
+++ src/app/admin/utilisateurs/profil/page.tsx
@@ -2,7 +2,7 @@ const Page = async () => {
 
     return (
         <main className="min-h-screen mt-16 px-4 custom-size-minmax">
-            <div>Ici théoriquement il serait pertinant d'avoir les infos de l'utilisateur ainsi que son historique de commandes</div>
+            <div>Ici théoriquement il serait pertinent d'avoir les infos de l'utilisateur ainsi que son historique de commandes</div>
         </main>
     );
 };
